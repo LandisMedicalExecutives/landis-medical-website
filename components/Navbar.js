6b23aee2e6e52d297d3c8210f1ba584e99f87719
LandisMedicalExecutives <lenglish6@gmail.com>
2026-03-24 13:43:23 -0400

    Delete Navbar component from the project
    Remove Navbar component implementation.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,1 @@
-import React from 'react';
 
-const Navbar = () => {
-    return (
-        <nav>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-            <button onClick={() => alert('Request Consultation')}>Request Consultation</button>
-        </nav>
-    );
-};
-
-export default Navbar;
