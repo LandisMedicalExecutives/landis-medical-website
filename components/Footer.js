600792327a5364b8d48380a72340c2b661f1beac
LandisMedicalExecutives <lenglish6@gmail.com>
2026-03-24 13:01:23 -0400

    Create Footer component with copyright information

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+// Footer.js
+
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2026 Landis Medical Executives. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
