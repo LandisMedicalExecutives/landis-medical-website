4ed3b345a9152e18915cb455eff501b2f4058670
LandisMedicalExecutives <lenglish6@gmail.com>
2026-03-24 13:57:28 -0400

    Convert MyDocument class to functional Document
    Refactor Document component to a functional component.

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,13 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-// This is the Document structure for the Next.js application
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps }; 
-  }
-
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet" />
-        </Head>
-        <body className="bg-gray-100">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
